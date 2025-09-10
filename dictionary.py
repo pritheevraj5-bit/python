00a5558a09b38a01dfd20f18535e6239a2c88940
pritheevraj5-bit <pritheevraj5@gmail.com>
2025-09-10 17:43:32 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+d={}
+n=int(input("enter the number of elements to be added in dictionary:"))
+for i in range (0,n):
+    key=input("enter key:")
+    value=input("enter value:")
+    d[key]=value
+print("the dictionary is",d)
