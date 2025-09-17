fade4570da765259f46b2f2c7b967217951c78dd
Pritheev <pritheevraj5@gmail.com>
2025-09-17 17:05:16 +0530

    Add files via upload

---
+++ turttle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.setup(300,200)
+window=turtle.Screen()
+window.title("my first turttle program")
+window.bgcolor("yellow")
+the_turtle=turtle.getturtle()
+the_turtle.hideturtle()
+the_turtle.setposition(50,35)
+the_turtle.setposition(25,50)
+the_turtle.setposition(-50,-25)
+the_turtle.setposition(0,0)
+
+turtle.exitonclick()
